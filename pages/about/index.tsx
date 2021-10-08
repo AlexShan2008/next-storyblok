41920a6180c6eb35e3cf47d07782939a2cad08ea
Alex Shan <shantong.sir@gmail.com>
2021-10-08 22:55:17 +0800

    feat: update data from Next.js API

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from "react";
+
 import type { NextPage } from "next";
 
 import Layout from "@components/Layout";
 
 const About: NextPage = () => {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("/api/hello")
+        .then((response) => response.json())
+        .then((res) => {
+          setName(res.name);
+        });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Layout>
-      <h3>About us</h3>
+      <h2>About us</h2>
       <p>This is a About page.</p>
       <p>Just test Merge request</p>
+
+      <br />
+
+      <section>
+        <h3>This data is from Next.js API:</h3>
+        <h2> {name}</h2>
+      </section>
     </Layout>
   );
 };
